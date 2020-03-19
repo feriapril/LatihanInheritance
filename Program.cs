cac2951411bb7d87853d9c8a129bb681e0ea667a
Fery Apriliawan <fery.apriliawan@students.amikom.ac.id>
2020-03-19 16:20:14 +0700

    Taxi V.1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace baru
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            taxi2 taxi = new taxi2();
+
+            taxi.Drivername = "Joko";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+
+        }
+    }
+}
